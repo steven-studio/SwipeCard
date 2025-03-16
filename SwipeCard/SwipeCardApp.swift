2a113e405d9ec8a7ebcb5914cb236724bcc8387a
游哲維 <stevenyu.supreme@gmail.com>
2025-03-16 16:14:27 +0800

    Initial Commit

---
+++ SwipeCard/SwipeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeCardApp.swift
+//  SwipeCard
+//
+//  Created by 游哲維 on 2025/3/16.
+//
+
+import SwiftUI
+
+@main
+struct SwipeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
